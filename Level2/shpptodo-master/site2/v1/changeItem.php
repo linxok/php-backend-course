205bb118f87bbdf43c7ef700636d7f5513d700a8
Vadym Kravchenko <vadym.kravchenko@skynix.co>
2021-10-18 00:22:06 +0300

    level 2 - 1

--- Level2/shpptodo-master/site2/v1/changeItem.php
+++ Level2/shpptodo-master/site2/v1/changeItem.php
@@ -1,1 +1,21 @@
 <?php
+require_once('config.php');
+
+$request = json_decode(file_get_contents('php://input'),true);
+
+if (is_array($request)){
+
+    $todo_list = json_decode(file_get_contents("todo.json"),true);
+
+    foreach ($todo_list['items'] as $item => $current){
+        if ($current['id'] == $request['id']){
+            $todo_list['items'][$item]['text'] = $request['text'];
+            $todo_list['items'][$item]['checked'] = $request['checked'];
+
+        }
+    }
+    file_put_contents("todo.json",json_encode($todo_list));
+
+   echo json_encode(["ok"=> true]);
+
+}
--- Level2/shpptodo-master/site2/v1/deleteItem.php
+++ Level2/shpptodo-master/site2/v1/deleteItem.php
@@ -1,1 +1,22 @@
 <?php
+require_once('config.php');
+
+$request = json_decode(file_get_contents('php://input'),true);
+
+if (is_array($request)){
+
+    $todo_list = json_decode(file_get_contents("todo.json"),true);
+
+    foreach ($todo_list['items'] as $item => $current){
+        if ($current['id'] == $request['id']){
+            unset($todo_list['items'][$item]);
+
+        }
+    }
+    $todo_list['items'] = array_values($todo_list['items']);
+
+    file_put_contents("todo.json",json_encode($todo_list));
+
+    echo json_encode(["ok"=> true]);
+
+}
