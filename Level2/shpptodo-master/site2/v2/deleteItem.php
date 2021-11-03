c9f8a01694991858aa7f4cd3442331332ce5ea43
Vadym Kravchenko <vadym.kravchenko@skynix.co>
2021-11-03 19:55:17 +0200

    next upp

--- Level2/shpptodo-master/site2/v2/deleteItem.php
+++ Level2/shpptodo-master/site2/v2/deleteItem.php
@@ -4,20 +4,18 @@ require_once 'db_connect.php';
 
 $request = json_decode(file_get_contents('php://input'),true);
 
-if (strlen($request['text'] ) > 0) {
+if (strlen($request['id'] ) > 0) {
 
     $id = $request['id'];
-    $text = $request['text'];
-    $checked = $request['checked'] ? 1 : 0;
 
-    $db_request = "UPDATE todolist SET text = '$text', checked = '$checked' WHERE id = '$id'";
+    $db_request = "DELETE FROM todolist WHERE id = '$id'";
 
     if (!$mysqli->query($db_request)){
 
         print "Invalid DB query";
         die("Invalid DB query");
     }
-    echo json_encode(array("id" => $id));
+    echo json_encode(array("ok" => true));
 }
 $mysqli->close();
 
